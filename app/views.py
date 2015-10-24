4a1a2c6b2381121985459dcb26084bf7c863c5b8
Bob D'Errico <bob@bobderrico.com>
2015-10-23 23:40:33 -0400

    Reports are now editable. Bootstrap fixes. Fixes decimal issue in database

--- app/views.py
+++ app/views.py
@@ -26,7 +26,48 @@ def submit():
                 print_speed=form.print_speed.data)
         db.session.add(j)
         db.session.commit()
-        flash('Report successfully submitted!')
+        flash('Report successfully saved!')
         return redirect(url_for('submit'))
 
-    return render_template('submit.html', title='Submit Report', form=form)
+    return render_template('submit.html',
+                           title='Submit Report',
+                           form=form,
+                           form_context = 'add',
+                           button_text='Submit Report')
+
+@app.route('/edit/<int:job_id>', methods=['GET', 'POST'])
+def edit(job_id):
+    form = NewJobForm()
+    job = Job.query.filter_by(id=job_id).first()
+
+    if form.validate_on_submit():
+        job = Job.query.filter_by(id=job_id).first()
+        job.job_name = form.job_name.data,
+        job.email = form.email.data,
+        job.infill = form.infill.data,
+        job.shells = form.shells.data,
+        job.layer_height = form.layer_height.data,
+        job.temperature = form.temperature.data,
+        job.extrude_speed = form.extrude_speed.data,
+        job.print_speed = form.print_speed.data
+
+        db.session.add(job)
+        db.session.commit()
+        flash('Report successfully updated!')
+        return redirect(url_for('view'))
+
+    form.job_name.data = job.job_name
+    form.email.data = job.email
+    form.infill.data = job.infill
+    form.shells.data = job.shells
+    form.layer_height.data = job.layer_height
+    form.temperature.data = job.temperature
+    form.extrude_speed.data = job.extrude_speed
+    form.print_speed.data = job.print_speed
+
+    return render_template('submit.html',
+                           title='Edit Report',
+                           job_id=job.id,
+                           form=form,
+                           form_context=edit,
+                           button_text='Save Changes')
